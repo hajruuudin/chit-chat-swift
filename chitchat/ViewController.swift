a23caf4c8eee9559bf0523b6c09caa5764ddb6b1
hajruuudin <hajruuudin@gmail.com>
2025-02-16 15:50:12 +0100

    Initial Commit

---
+++ chitchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chitchat
+//
+//  Created by Hajrudin Imamovic on 16/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ chitchatTests/chitchatTests.swift
@@ -0,0 +1,17 @@
+//
+//  chitchatTests.swift
+//  chitchatTests
+//
+//  Created by Hajrudin Imamovic on 16/02/2025.
+//
+
+import Testing
+@testable import chitchat
+
+struct chitchatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
